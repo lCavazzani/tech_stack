2b31c8ab2c283956646a5136cc46b04e2c9ffb6c
leonardoCavazzani <cavazzanileonardo@gmail.com>
2018-10-12 16:17:15 -0500

    animation

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
-import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { 
+    Text, 
+    TouchableWithoutFeedback, 
+    View, 
+    LayoutAnimation 
+} from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
 
+    componentWillUpdate() {
+        LayoutAnimation.spring();
+    }
+
     renderDescription() {
         if (this.props.expanded) {
             return (
-            <Text>{this.props.library.item.description}</Text>
+            <CardSection>
+                <Text>{this.props.library.item.description}</Text>
+            </CardSection>
             );
         }
     }
